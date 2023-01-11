dfb3ae6ed2a372accd80b1ab8883645f9192f9e2
Nico Foth <nico@foth.dev>
2023-01-11 11:34:24 +0100

    some "progress" in 2022 day18 part two

--- 2022/Day18/partTwo.py
+++ 2022/Day18/partTwo.py
@@ -19,20 +19,74 @@ def partTwo():
                 adjacent_cubes[cube].append(cube2)
 
     air_cubes = defaultdict(list)
-    # Placing air cubes
     air_cube_placed_next_to_obsidian = True
     while air_cube_placed_next_to_obsidian:
         air_cube_placed_next_to_obsidian = False
         for cube in adjacent_cubes:
-            if adjacent_cubes[cube] < 6:
-                # Add air cube to air cubes on positions where a side of the cube is exposed to air
+            if len(adjacent_cubes[cube]) < 6:
+                # Find out which side is exposed to air (e.g. which positions)
+                possible_positions = [
+                    (cube[0]+1, cube[1], cube[2]),
+                    (cube[0]-1, cube[1], cube[2]),
+                    (cube[0], cube[1]+1, cube[2]),
+                    (cube[0], cube[1]-1, cube[2]),
+                    (cube[0], cube[1], cube[2]+1),
+                    (cube[0], cube[1], cube[2]+1),
+                ]
                 # Also check if the cube currently looked at is obsidian to keep the while loop running
-                pass
+                for pos in possible_positions:
+                    if pos in input:
+                        air_cube_placed_next_to_obsidian = True
+                
+                exposed_positions = [x for x in possible_positions if x not in adjacent_cubes[cube]]
+                
+
+                # Add air cube to air cubes on positions where a side of the cube is exposed to air
+                for pos in exposed_positions:
+                    adjacent_cubes[cube].append(pos)
+                    air_cubes[pos] = [x for x in possible_positions if x in adjacent_cubes]
+        for key, value in air_cubes.items():
+            adjacent_cubes[key] = value
+                
     
     # Remove air cubes if they are exposed to air until no cubes are removed
+    removed = True
+    while removed:
+        removed = False
+        to_be_removed = []
+        for cube in adjacent_cubes:
+            if cube in input:
+                continue
+            for adj_cube in adjacent_cubes[cube]:
+                if adj_cube in input:
+                    break
+            else:
+                possible_positions = [
+                    (cube[0]+1, cube[1], cube[2]),
+                    (cube[0]-1, cube[1], cube[2]),
+                    (cube[0], cube[1]+1, cube[2]),
+                    (cube[0], cube[1]-1, cube[2]),
+                    (cube[0], cube[1], cube[2]+1),
+                    (cube[0], cube[1], cube[2]+1),
+                    ]
+                if [x for x in possible_positions if x not in adjacent_cubes[cube]]:
+                    to_be_removed.append(cube)
+        if to_be_removed:
+            removed = True
+        for deletion in to_be_removed:
+            adjacent_cubes.pop(deletion)
+            for cube in adjacent_cubes:
+                if deletion in adjacent_cubes[cube]:
+                    adjacent_cubes[cube].remove(deletion)
+
 
     # Count the unconnected sides of the obsidian cubes (input list)
-
+    for cube in adjacent_cubes:
+        if cube in adjacent_cubes:
+            unconnected_sides += 6-len(adjacent_cubes[cube])
+        else:
+            unconnected_sides += 6
+    print(unconnected_sides)
 
 
 partTwo()
