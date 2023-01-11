e0b0512c8908e61317e1292bebe715534c774441
Nico Foth <nico@foth.dev>
2023-01-11 22:14:34 +0100

    Finished 2022 Day18 part two

--- 2022/Day18/partTwo.py
+++ 2022/Day18/partTwo.py
@@ -1,92 +1,69 @@
-from collections import defaultdict
+from collections import deque
 
 with open("2022/Day18/input.txt") as inputFile:
     input = [tuple(int(y) for y in x.split(",")) for x in inputFile.read().splitlines()]
 
+all_cubes = set()
+for cube in input:
+    all_cubes.add(cube)
+max_coord = max([max([cube[0] for cube in all_cubes]), max([cube[1] for cube in all_cubes]), max([cube[2] for cube in all_cubes])])
+min_coord = min([min([cube[0] for cube in all_cubes]), min([cube[1] for cube in all_cubes]), min([cube[2] for cube in all_cubes])])
+
+
+def getPositions(cube):
+    possible_positions = [
+        (cube[0]+1, cube[1], cube[2]),
+        (cube[0]-1, cube[1], cube[2]),
+        (cube[0], cube[1]+1, cube[2]),
+        (cube[0], cube[1]-1, cube[2]),
+        (cube[0], cube[1], cube[2]+1),
+        (cube[0], cube[1], cube[2]-1),
+    ]
+    return possible_positions
+
+
+def reaches_outside(cube):
+    x, y, z = cube
+
+    seen = set()
+    queue = deque([(x,y,z)])
+
+    while queue:
+        x,y,z = queue.popleft()
+        
+        if (x,y,z) in all_cubes:
+            continue
+        if (x,y,z) in seen:
+            continue
+        
+        seen.add((x,y,z))
+        for coord in (x,y,z):
+            if coord > max_coord or coord < min_coord:
+                return True
+        
+        for adj in getPositions((x,y,z)):
+            queue.append(adj)
+
+    return False
 
 def partTwo():
-    adjacent_cubes = defaultdict(list)
-    unconnected_sides = 0
-    for cube in input:
-        for cube2 in input:
-            if cube == cube2:
-                continue
-            if abs(cube[0]-cube2[0]) == 1 and cube[1] == cube2[1] and cube[2] == cube2[2]:
-                adjacent_cubes[cube].append(cube2)
-            elif abs(cube[1]-cube2[1]) == 1 and cube[0] == cube2[0] and cube[2] == cube2[2]:
-                adjacent_cubes[cube].append(cube2)
-            elif abs(cube[2]-cube2[2]) == 1 and cube[0] == cube2[0] and cube[1] == cube2[1]:
-                adjacent_cubes[cube].append(cube2)
+    surface_area = 0
 
-    air_cubes = defaultdict(list)
-    air_cube_placed_next_to_obsidian = True
-    while air_cube_placed_next_to_obsidian:
-        air_cube_placed_next_to_obsidian = False
-        for cube in adjacent_cubes:
-            if len(adjacent_cubes[cube]) < 6:
-                # Find out which side is exposed to air (e.g. which positions)
-                possible_positions = [
-                    (cube[0]+1, cube[1], cube[2]),
-                    (cube[0]-1, cube[1], cube[2]),
-                    (cube[0], cube[1]+1, cube[2]),
-                    (cube[0], cube[1]-1, cube[2]),
-                    (cube[0], cube[1], cube[2]+1),
-                    (cube[0], cube[1], cube[2]+1),
-                ]
-                # Also check if the cube currently looked at is obsidian to keep the while loop running
-                for pos in possible_positions:
-                    if pos in input:
-                        air_cube_placed_next_to_obsidian = True
-                
-                exposed_positions = [x for x in possible_positions if x not in adjacent_cubes[cube]]
-                
-
-                # Add air cube to air cubes on positions where a side of the cube is exposed to air
-                for pos in exposed_positions:
-                    adjacent_cubes[cube].append(pos)
-                    air_cubes[pos] = [x for x in possible_positions if x in adjacent_cubes]
-        for key, value in air_cubes.items():
-            adjacent_cubes[key] = value
-                
+    for (x,y,z) in all_cubes:
+        if reaches_outside((x+1,y,z)):
+            surface_area += 1
+        if reaches_outside((x-1,y,z)):
+            surface_area += 1
+        if reaches_outside((x,y+1,z)):
+            surface_area += 1
+        if reaches_outside((x,y-1,z)):
+            surface_area += 1
+        if reaches_outside((x,y,z+1)):
+            surface_area += 1
+        if reaches_outside((x,y,z-1)):
+            surface_area += 1
     
-    # Remove air cubes if they are exposed to air until no cubes are removed
-    removed = True
-    while removed:
-        removed = False
-        to_be_removed = []
-        for cube in adjacent_cubes:
-            if cube in input:
-                continue
-            for adj_cube in adjacent_cubes[cube]:
-                if adj_cube in input:
-                    break
-            else:
-                possible_positions = [
-                    (cube[0]+1, cube[1], cube[2]),
-                    (cube[0]-1, cube[1], cube[2]),
-                    (cube[0], cube[1]+1, cube[2]),
-                    (cube[0], cube[1]-1, cube[2]),
-                    (cube[0], cube[1], cube[2]+1),
-                    (cube[0], cube[1], cube[2]+1),
-                    ]
-                if [x for x in possible_positions if x not in adjacent_cubes[cube]]:
-                    to_be_removed.append(cube)
-        if to_be_removed:
-            removed = True
-        for deletion in to_be_removed:
-            adjacent_cubes.pop(deletion)
-            for cube in adjacent_cubes:
-                if deletion in adjacent_cubes[cube]:
-                    adjacent_cubes[cube].remove(deletion)
-
-
-    # Count the unconnected sides of the obsidian cubes (input list)
-    for cube in adjacent_cubes:
-        if cube in adjacent_cubes:
-            unconnected_sides += 6-len(adjacent_cubes[cube])
-        else:
-            unconnected_sides += 6
-    print(unconnected_sides)
+    print(surface_area)
 
 
 partTwo()
