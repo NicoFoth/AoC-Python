e0a4b0f139fb3905b044f301f27b12d23a1ffd55
Nico Foth <nico@foth.dev>
2023-01-20 03:11:15 +0100

    Solved 2021/Day5 part two, optimized part one

--- 2021/Day5/partOne.py
+++ 2021/Day5/partOne.py
@@ -10,25 +10,22 @@ def partOne():
     for path in input:
         complete_path = path
         path = path.split(" -> ")
-        coord1 = path[0].split(",")
-        coord2 = path[1].split(",")
+        path = [pos.split(",") for pos in path]
+        one = [int(x) for x in path[0]]
+        two = [int(x) for x in path[1]]
 
-        if not coord1[0] == coord2[0] and not coord1[1] == coord2[1]:
+        if not one[0] == two[0] and not one[1] == two[1]:
             continue
 
-        path = [pos.split(",") for pos in path]
-        for i in range(1):
-            one = [int(x) for x in path[i]]
-            two = [int(x) for x in path[i+1]]
-            environment[tuple(one)].append(complete_path)
-            for coord in range(2):
-                for k in range(abs(one[coord]-two[coord])):
-                    new = one.copy()
-                    if two[coord] > one[coord]:
-                        new[coord] += k+1
-                    else:
-                        new[coord] -= k+1
-                    environment[tuple(new)].append(complete_path)
+        environment[tuple(one)].append(complete_path)
+        for coord in range(2):
+            for k in range(abs(one[coord]-two[coord])):
+                new = one.copy()
+                if two[coord] > one[coord]:
+                    new[coord] += k+1
+                else:
+                    new[coord] -= k+1
+                environment[tuple(new)].append(complete_path)
 
     counter = 0
     for key, value in environment.items():
---
+++ 2021/Day5/partTwo.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+
+with open("2021/Day5/input.txt") as inputFile:
+    input = [line for line in inputFile.read().splitlines()]
+
+
+def partTwo():
+    environment = defaultdict(list)
+    for path in input:
+        complete_path = path
+        path = path.split(" -> ")
+        path = [pos.split(",") for pos in path]
+        one = [int(x) for x in path[0]]
+        two = [int(x) for x in path[1]]
+
+        if not one[0] == two[0] and not one[1] == two[1]:
+            environment[tuple(one)].append(complete_path)
+            for k in range(abs(one[0]-two[0])):
+                new = one.copy()
+                for i in range(2):
+                    if two[i] > one[i]:
+                        new[i] += k+1
+                    else:
+                        new[i] -= k+1
+                environment[tuple(new)].append(complete_path)
+
+        else:
+            environment[tuple(one)].append(complete_path)
+            for coord in range(2):
+                for k in range(abs(one[coord]-two[coord])):
+                    new = one.copy()
+                    if two[coord] > one[coord]:
+                        new[coord] += k+1
+                    else:
+                        new[coord] -= k+1
+                    environment[tuple(new)].append(complete_path)
+
+    counter = 0
+    for key, value in environment.items():
+        if len(value) >= 2:
+            counter += 1
+
+    print(counter)
+
+
+partTwo()
