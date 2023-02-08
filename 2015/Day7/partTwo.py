dd5ad3f4b3be4c7146b97045c52105c8a7585de0
Nico Foth <nico@foth.dev>
2023-02-08 16:00:25 +0100

    Solved 2015 Day7 part two

---
+++ 2015/Day7/partTwo.py
@@ -0,0 +1,45 @@
+import functools
+
+
+with open("2015/Day7/input.txt", encoding="utf-8") as inputFile:
+    input = inputFile.read().splitlines()
+    definitions = {line.split(" -> ")[1]: line.split(" -> ")[0] for line in input}
+
+
+@functools.lru_cache
+def calculate(definition):
+    if definition.isnumeric():
+        return int(definition)
+    
+    operation = definitions[definition]
+    if operation.isnumeric():
+        return int(operation)
+    
+    operation = operation.split(" ")
+    if len(operation) == 1:
+        return calculate(operation[0])
+
+    elif len(operation) == 2:
+        return 2**16-1 - calculate(operation[1])
+
+    if operation[1] == "AND":
+        return calculate(operation[0]) & calculate(operation[2])
+    elif operation[1] == "OR":
+        return calculate(operation[0]) | calculate(operation[2])
+    elif operation[1] == "XOR":
+        return calculate(operation[0]) ^ calculate(operation[2])
+    elif operation[1] == "LSHIFT":
+        return calculate(operation[0]) << int(operation[2])
+    elif operation[1] == "RSHIFT":
+        return calculate(operation[0]) >> int(operation[2])
+
+
+def partTwo():
+    result = calculate("a")
+    definitions["b"] = str(result)
+    calculate.cache_clear()
+    result = calculate("a")
+    print(result)
+
+
+partTwo()
