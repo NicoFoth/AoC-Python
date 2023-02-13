d73a7e4b79462fb4f9ac9c81ab8fe5679876d5ec
Nico Foth <nico@foth.dev>
2023-02-13 20:02:29 +0100

    Solved 2015 day9 part one

--- 2015/Day9/partOne.py
+++ 2015/Day9/partOne.py
@@ -1,13 +1,26 @@
+from itertools import permutations
+
 with open("2015/Day9/input.txt") as inputFile:
     input = inputFile.read().splitlines()
-    distances = [int(x.split("= ")[1]) for x in input]
+    locations = set()
+    distances = dict()
+    for line in input:
+        source, _, destination, _, distance = line.split()
+        locations.add(source)
+        locations.add(destination)
+
+        distances.setdefault(source, dict())[destination] = int(distance)
+        distances.setdefault(destination, dict())[source] = int(distance)
 
 
 def partOne():
-    distance_combinations = []
-    for x in range(len(distances)):
-        for y in range(x+1, len(distances)):
-            distance_combinations.append(distances[x]+distances[y])
-    print(min(distance_combinations))
+    solution = []
+    
+    for perm in permutations(locations):
+        distance = sum(map(lambda x, y: distances[x][y], perm[:-1], perm[1:]))
+        solution.append(distance)
+
+    print(min(solution))
+
 
 partOne()
