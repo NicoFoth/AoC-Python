58a0c291956cfa9d4430d15d09867fd51fff3112
Nico Foth <nico@foth.dev>
2023-02-13 23:25:31 +0100

    Solved 2015 Day10 part one and two

---
+++ 2015/Day10/partTwo.py
@@ -0,0 +1,13 @@
+from itertools import groupby
+
+with open("2015/Day10/input.txt", "r") as inputFile:
+    input = inputFile.read().replace("\n", "")
+    
+
+def partOne():
+    new_string = input
+    for _ in range(50):
+        new_string = "".join([str(len(list(iterator))) + char for char, iterator in groupby(new_string)])
+    print(len(new_string))
+
+partOne()
